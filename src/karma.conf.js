4add9c9cec0faaa0c6da29b78b5de9b1759ea880
fstbraz <fst.braz@gmail.com>
2019-03-17 13:34:59 +0000

    config(karma): adding proxy config

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -27,6 +27,13 @@ module.exports = function(config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    proxies: {
+      '/api': {
+        target: 'http://anapioficeandfire.com/api',
+        secure: false,
+        changeOrigin: true
+      }
+    }
   });
 };
